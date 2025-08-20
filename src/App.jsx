4658a0b9e7b3cca3259e8ac436f2819598e49393
TomTom1194 <tangthientan10194@gmail.com>
2025-08-20 18:10:34 +0700

    update43

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contactus";
 import About from "./pages/Aboutus";
